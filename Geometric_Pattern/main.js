384ec9c5ebe794065497eef5cd321a1557600832
Halei Belvail <h2dabzay@gmail.com>
2017-04-18 19:51:35 +0000

    Add colors to Geometric_Pattern.

--- Geometric_Pattern/main.js
+++ Geometric_Pattern/main.js
@@ -1,6 +1,9 @@
 var NUM_CIRCLES = 12;
-var CD
+var CD;
 var CR;
+var rVal;
+var bVal;
+var gVal;
 
 
 
@@ -13,6 +16,10 @@ CR = CD/2;
 
 
 function draw() {
+    rVal = 255;
+    gVal = 50;
+    bVal = 90;
+    
     var isShifted = false;
   var y = height;
   while( y >= 0){
@@ -25,12 +32,25 @@ function draw() {
  }
  
   while (x <= width) {
+      
+      fill(color(rVal,bVal,gVal));
+      stroke(color(bVal,gVal,rVal));
+      
+     
       ellipse(x,y,CD,CD);
       x = x + CD;
+  
   }
   y = y-CR;
-  isShifted = !isShifted
+  isShifted = !isShifted;
+      
+      rVal = rVal-2;
+      bVal = bVal+3;
+      gVal = gVal+7;
+  
+  }
+ 
+  
+  
       
   }
-
-}
